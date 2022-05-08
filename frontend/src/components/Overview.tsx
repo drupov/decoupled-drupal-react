c09c2a3e89916af9118e43944525b428dc815d49
Dimitar Rupov <dimitar.rupov@gmail.com>
2022-05-08 21:14:26 +0300

    Show CreatePlayer component only for authenticated users

--- frontend/src/components/Overview.tsx
+++ frontend/src/components/Overview.tsx
@@ -1,11 +1,14 @@
 import Players from './Players'
 import CreatePlayer from './CreatePlayer'
+import { useAuthenticated } from 'drupal-react-oauth-provider'
 
 const Overview = () => {
+  const isAuthenticated = useAuthenticated()
+
   return (
     <>
+      {isAuthenticated && <CreatePlayer />}
       <Players />
-      <CreatePlayer />
     </>
   )
 }
