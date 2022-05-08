2952a3ceaa47c33a7cf0cab622af5a6bf97d6f55
Dimitar Rupov <dimitar.rupov@gmail.com>
2022-05-09 00:17:38 +0300

    Improve Players component

--- frontend/src/components/Players.tsx
+++ frontend/src/components/Players.tsx
@@ -6,21 +6,21 @@ import { PLAYERS } from '../gql/common'
 const Players: any = () => {
   const context: any = useContext(PlayerContext)
   const {data, loading, error} = useQuery(PLAYERS)
-  
+
   if (error) {
-    return error.message
+    return <p>{error.message}</p>
   }
+
+  const players = data?.players?.items?.length ? data.players.items.map((item: any) => (
+    <button key={item.id} onClick={() => context.setActivePlayerId(item.id)}>
+      {item.firstName} {item.lastName} ({item.id})
+    </button>
+  )) : <p>No players data available</p>
 
   return (
     <>
-      <h1>Players</h1>
-      {loading ? <p>Loading...</p> :
-        data.players.items.map((item: any) => (
-          <button key={item.id} onClick={() => context.setActivePlayerId(item.id)}>
-            {item.firstName} {item.lastName} ({item.id})
-          </button>
-        ))
-      }
+      <h2>Players</h2>
+      {loading ? <p>Loading...</p> : players}
     </>
   )
 }
