76bdb1b689d870ba73e074709ba7e5fd9589e10d
Dimitar Rupov <dimitar.rupov@gmail.com>
2022-05-08 21:15:09 +0300

    Handle login in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,29 @@
-import {useState} from 'react'
+import { useState } from 'react'
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries'
+import { sha256 } from 'crypto-hash'
+import Login from './components/Login'
+import PlayerContext from './context/PlayerContext'
 import Overview from './components/Overview'
 import Player from './components/Player'
-import PlayerContext from './context/PlayerContext'
-import {HttpLink, InMemoryCache, ApolloClient, ApolloProvider} from '@apollo/client'
-import {createPersistedQueryLink} from '@apollo/client/link/persisted-queries'
-import {sha256} from 'crypto-hash'
 
-const httpLink = new HttpLink({uri: 'https://decoupled-drupal-react.lndo.site/graphql'})
-const persistedQueriesLink = createPersistedQueryLink({ sha256 })
+const persistedQueriesLink = createPersistedQueryLink({sha256})
+const httpLink = createHttpLink({uri: 'https://decoupled-drupal-react.lndo.site/graphql'})
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${JSON.parse(token).access_token}` : '',
+    }
+  }
+});
 
 const client: any = new ApolloClient({
   cache: new InMemoryCache(),
-  link: persistedQueriesLink.concat(httpLink)
+  link: persistedQueriesLink.concat(authLink).concat(httpLink)
 })
 
 const App = () => {
@@ -23,7 +35,10 @@ const App = () => {
         activePlayerId: activePlayerId,
         setActivePlayerId: setActivePlayerId
       }}>
-        {activePlayerId ? <Player /> : <Overview /> }
+        {activePlayerId ? <Player /> : <Overview />}
+        <div>
+          <Login />
+        </div>
       </PlayerContext.Provider>
     </ApolloProvider>
   )
