2392bf1d96303eb3c25aa8634828e19ee17baafc
Dimitar Rupov <dimitar.rupov@gmail.com>
2022-05-08 21:13:53 +0300

    Wrap app in DrupalProvider and update index.tsx for latest React

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { DrupalProvider } from 'drupal-react-oauth-provider'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')!
+const root = createRoot(rootElement)
+
+const config = {
+  url: 'https://decoupled-drupal-react.lndo.site/'
+}
+
+root.render(
+  <StrictMode>
+    <DrupalProvider config={config}>
+      <App />
+    </DrupalProvider>
+  </StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
