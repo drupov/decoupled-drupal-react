953e353aaf3f5a1b27950cbb6084c2296f7dbcc0
Dimitar Rupov <dimitar.rupov@gmail.com>
2022-05-09 00:17:10 +0300

    Improve Login component

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,57 +2,67 @@ import { useLazyLogin, useLazyLogout, useAuthenticated } from 'drupal-react-oaut
 import { useApolloClient } from '@apollo/client'
 
 const Login = () => {
+  // Run the 'drupal-react-oauth-provider' hooks.
   const isAuthenticated = useAuthenticated()
-
   const [login, {loading, error, data}] = useLazyLogin()
   const [logout] = useLazyLogout()
+
+  // Make logout async, so that the store clearance can be done only after a successful logout.
   const logoutUser = async () => {
     logout();
   }
+
+  // Get the client, so that the store can be cleared after the logout.
   const client = useApolloClient()
 
-  if (!isAuthenticated) {
-    const client_id = process.env.REACT_APP_OAUTH_CLIENT_ID ?? ''
-    const client_secret = process.env.REACT_APP_OAUTH_CLIENT_SECRET ?? ''
-    const grant_type = process.env.REACT_APP_OAUTH_GRANT_TYPE ?? ''
-    const scope = process.env.REACT_APP_OAUTH_SCOPE ?? ''
+  // Get oauth data from the environment variables.
+  const client_id = process.env.REACT_APP_OAUTH_CLIENT_ID ?? ''
+  const client_secret = process.env.REACT_APP_OAUTH_CLIENT_SECRET ?? ''
+  const grant_type = process.env.REACT_APP_OAUTH_GRANT_TYPE ?? ''
+  const scope = process.env.REACT_APP_OAUTH_SCOPE ?? ''
 
-    return (
-      <form onSubmit={(event: any) => {
-        event.preventDefault()
-        const username = event.target.username.value
-        const password = event.target.password.value
-
-        login({
-          username,
-          password,
-          client_id,
-          client_secret,
-          grant_type,
-          scope
-        })
-      }}>
-        <label htmlFor="username">Username</label>
-        <input type="text" name="username" />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" />
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    )
+  if (data) {
+    localStorage.setItem('token', JSON.stringify(data))
   }
-  else {
-    if (data) {
-      localStorage.setItem('token', JSON.stringify(data))
-    }
 
-    return (
-      <button onClick={() => {
-        logoutUser().then(() => client.clearStore())
-      }}>Logout</button>
-    )
-  }
+  return (
+    <>
+      {!isAuthenticated &&
+        <form onSubmit={(event: any) => {
+          event.preventDefault()
+          const username = event.target.username.value
+          const password = event.target.password.value
+
+          login({
+            username,
+            password,
+            client_id,
+            client_secret,
+            grant_type,
+            scope
+          })
+        }}>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" />
+          <br />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      }
+
+      {loading && <p>Loading...</p>}
+
+      {error && <p>{error.message}</p>}
+
+      {isAuthenticated && (
+        <button onClick={() => {
+          logoutUser().then(() => client.clearStore())
+        }}>Logout</button>
+      )}
+    </>
+  )
 }
 
 export default Login
